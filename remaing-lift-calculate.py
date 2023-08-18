58d602c3fd2bfa482b63d3c2d25fa73ed6acd675
rbhavanishankar1 <130890749+rbhavanishankar1@users.noreply.github.com>
2023-08-18 09:38:23 +0530

    Add files via upload

---
+++ remaing-lift-calculate.py
@@ -0,0 +1,8 @@
+#calculate the how many months and how many weeks or 
+# how many days lest for dead if we have the 90 year of life span
+age=int(input("what is your current age ?"))
+Days=(90-age)*365
+Weaks=(90-age)*52
+Months=(90-age)*12
+Years=90-age
+print("you have {0} Days,{1} Weaks,{2} Months,{3} Years".format(Days,Weaks,Months,Years))
